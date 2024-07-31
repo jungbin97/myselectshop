a2637db2ba74efe0ee52dda92ebcdc93efa75856
jungbin97 <ljb5054@gmail.com>
2024-07-31 13:40:26 +0900

    관심상품 등록 API

---
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -0,0 +1,24 @@
+package com.sparta.myselectshop.controller;
+
+import com.sparta.myselectshop.dto.ProductRequestDto;
+import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping("/products")
+    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto) {
+        return productService.createProduct(requestDto);
+    }
+
+    // 관심상품 희망 최저가 등록하기
+//    @PutMapping("/products/{id}")
+//    public ProductResponseDto updateProduct(@PathVariable Long id, )save
+}
